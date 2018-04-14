53130a0d7d35bea11bce7709a19064fca045dd7f
Teklehaimanot Derso <teklehaimanot@gmail.com>
2018-04-14 22:08:29 +0300

    Added a constant message and print it.

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let message = "Hello Git!"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(message)
     }
 
     override func didReceiveMemoryWarning() {
