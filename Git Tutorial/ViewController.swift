2445d8e0a3035a3a3cfaaba7e6022a06b6e859fd
Teklehaimanot Derso <teklehaimanot@gmail.com>
2018-04-14 23:02:45 +0300

    Add a function reverse.

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -16,11 +16,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print(message)
+        print(reverse(text: "stressed"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func reverse(text: String) -> String {
+        return String(text.reversed())
     }
 
 
